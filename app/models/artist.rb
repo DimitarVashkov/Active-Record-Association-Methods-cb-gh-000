4f650aba48c297a27bf541ae62562558db6985fd
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-01 16:01:37 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Artist.songs.first.genre.name
+    self.songs.first.genre.name
     #return the genre of the artist's first saved song
   end
 
